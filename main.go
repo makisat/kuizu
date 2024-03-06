45db31802e93d2bcb9b38f822d3c05a030ef0b90
makisat <makiya28sato@gmail.com>
2024-03-06 16:11:49 -0500

    add new user

--- main.go
+++ main.go
@@ -29,6 +29,8 @@ func main() {
 
     app.Get("/get-user/:id", getUser)
 
+    app.Post("/add-user", addUser)
+
     log.Fatal(app.Listen(":8080"))
 }
 
@@ -36,21 +38,39 @@ func main() {
 // delete quiz
 // get users
 func getUser(c *fiber.Ctx) error {
-    rows, err := db.Query(fmt.Sprintf("SELECT * FROM users WHERE user_id=%v;", c.Params("id")))
+    rows, err := db.Query(fmt.Sprintf("SELECT * FROM users WHERE user_id=%v", c.Params("id")))
     if err != nil {
         log.Fatal("err at query: " + err.Error())
     }
 
+    user := new(User)
+
     for rows.Next() {
-        var id int
-        var username string
-        var password string
-        rows.Scan(&id, &username, &password)
-        fmt.Println("id:", id)
-        fmt.Println("username:", username)
-        fmt.Println("password:", password)
+        rows.Scan(&user.UserId, &user.Username, &user.Password)
     }
 
-    return c.SendString("success")
+    return c.JSON(user)
+}
 
+func addUser(c *fiber.Ctx) error {
+
+    new_user := &struct {
+        Username string `json:"username"`
+        Password string `json:"password"`
+    } {}
+
+    err := c.BodyParser(new_user)
+    if err != nil {
+        log.Fatal("err at bodyparser: " + err.Error())
+    }
+
+    fmt.Println("new_user: ", new_user.Username)
+
+    _, err = db.Exec(fmt.Sprintf("INSERT INTO users (username, password) VALUES ('%s', '%s')", new_user.Username, new_user.Password))
+    if err != nil {
+        log.Fatal("err at inserting data: " + err.Error())
+        return c.SendStatus(fiber.StatusBadRequest)
+    }
+
+    return c.SendStatus(fiber.StatusOK)
 }
